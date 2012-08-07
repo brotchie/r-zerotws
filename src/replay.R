dd5f3ee79c443bde15cbe2c4168173b935d2ee74
James Brotchie <brotchie@gmail.com>
2012-08-07 22:21:56 +1000

    replay.R now reports the observed and unobserved message types.

--- src/replay.R
+++ src/replay.R
@@ -8,8 +8,20 @@ engine <- SQLite()
 db <- dbConnect(engine, DATABASE.PATH)
 messages <- dbGetQuery(db, 'SELECT * FROM messages')
 
+observed <- list()
+
 for (rawmsg in messages$content) {
     fields <- parseFields(rawmsg)
     msg <- IB.Message.parse(fields)
+    observed <- c(observed, .int(fields[1]))
     print(msg)
 }
+
+
+unobserved <- unlist(setdiff(messages.native, observed))
+
+
+print('Observed Message Types')
+print(names(messages.native)[messages.native %in% observed])
+print('Unobserved Message Types')
+print(names(messages.native)[messages.native %in% unobserved])
