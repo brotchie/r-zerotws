deba8dfdc88b91f193c9ae74d7ce0a5dea209806
James Brotchie <brotchie@gmail.com>
2012-08-08 22:12:29 +1000

    Added correct copyright notice.

--- src/ribtws.R
+++ src/ribtws.R
@@ -1,7 +1,7 @@
 ##  ___                  _                                                   ##
 ## | _ \  ______ _ _ ___| |___ __ _____ r-ØTWS - Comms Between R and an IB   ##
 ## |   /_|_ / -_) '_/ _ \  _\ V  V (_-<          TWS API <-> ØMQ Proxy       ##
-## |_|_(_)__\___|_| \___/\__|\_/\_//__/ (c) 2012, James Brotchie             ##
+## |_|_(_)__\___|_| \___/\__|\_/\_//__/ Copyright © 2012, James Brotchie     ##
 ##                                      http://zerotick.org/                 ##
 
 library(rzmq)
